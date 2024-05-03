b9e9e8687ea6986154c05f51eb00ae6b46d29f72
Seth Ketsi <christambassador12@gmail.com>
2024-05-03 21:10:27 +0000

    updated

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -13,7 +13,7 @@ def index_range(page, page_size):
     those particular pagination parameters.
     """
     start = (page - 1) * page_size
-    end = start + page_size
+    end = start * page_size
     return start, end
 
 
