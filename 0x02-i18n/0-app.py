ae7ac96d64b0ae47f2ee18ba38284544840b5657
Seth Ketsi <christambassador12@gmail.com>
2024-05-14 18:06:48 +0000

    updated

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-
+"""Basic Flask app"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """
+    Render the index page.
+    """
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
