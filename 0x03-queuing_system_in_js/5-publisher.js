75bd13ca1532380515b5570ee7f75f66704dfdc6
Seth Ketsi <christambassador12@gmail.com>
2024-06-11 21:13:32 +0000

    5. Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) =>
+    console.log('Redis client not connected to the server', err)
+);
+
+client.on('connect', () =>
+    console.log('Redis client connected to the server')
+);
+
+
+const publishMessage = (msg, time) => {
+    setTimeout(() => {
+        console.log(`About to send ${msg}`);
+        client.publish('holberton school channel', msg);
+    }, time);
+};
+
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) =>
+    console.log('Redis client not connected to the server', err)
+);
+
+client.on('connect', () =>
+    console.log('Redis client connected to the server')
+);
+
+client.subscribe('holberton school channel');
+
+client.on('message', (err, msg) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(msg);
+    if (msg === 'KILL_SERVER') {
+        client.unsubscribe();
+        client.quit();
+    }
+});
